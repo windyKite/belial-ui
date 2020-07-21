bb787400842f41be047d205b922f038d50b61f50
萧泳森 <603540681@qq.com>
2020-07-21 13:37:12 +0800

    feat: webpack.config.js 添加 externals 排除打包 react 和 react-dom

--- webpack.config.js
+++ webpack.config.js
@@ -33,4 +33,18 @@ module.exports = {
       template: 'index.html'
     })
   ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    }
+  }
 }
