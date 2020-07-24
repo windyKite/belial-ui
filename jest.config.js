81d15b3a2cb6a5d1240992e1c9c5d37d223fc83b
萧泳森 <603540681@qq.com>
2020-07-24 16:56:17 +0800

    feat: 测试覆盖率配置

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+// https://jestjs.io/docs/en/configuration.html
 module.exports = {
   verbose: true,
   transform: { // 配置编译器转换(相应文件对应相应转换器)
@@ -9,4 +10,19 @@ module.exports = {
     "\\.(css|s[ac]ss)$": "<rootDir>/test/__mocks__/styleMock.js"
   },
   setupFilesAfterEnv: ["<rootDir>test/setupTests.js"],  // enzyme 的设置文件路径
+
+  // 测试报告收集
+  collectCoverage: true, // 开启测试覆盖率,生成 coverage 目录
+  collectCoverageFrom: ['lib/**/*.{ts,tsx}', '!**/node_modules/**'],
+  coverageReporters: ["lcov", "text"],
+  reporters: ["default"], 
+
+  moduleFileExtensions: ['tsx', 'ts', 'js', 'jsx'],
+  modulePathIgnorePatterns: ['/style/'], // 忽略 style 文件夹下的文件测试
+
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',  // 配置 typescript 编译器相关的配置
+    }
+  }
 };
