62d9411a9b1cb70d04f50965b552acc5eafef047
萧泳森 <603540681@qq.com>
2020-07-29 17:14:21 +0800

    feat: 修改 circleci 的 yml 配置, 配置 jest 的 ci 配置

---
+++ jest.config.ci.js
@@ -0,0 +1,14 @@
+const baseJestConfig = require("./jest.config");
+
+module.exports = Object.assign({}, baseJestConfig, {
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: ".test-results/jest",
+        outputName: "results.xml",
+      },
+    ]
+  ] // jest-junit 生成 xml 测试报表(默认路径是.test-results/jest/results.xml)
+})
