bf8859345ba361454a9b7d76395931ee425de5d0
萧泳森 <603540681@qq.com>
2020-08-10 14:17:26 +0800

    feat: icon 组件增加旋转功能

--- lib/icon/icon.tsx
+++ lib/icon/icon.tsx
@@ -1,21 +1,34 @@
-
-import * as React from 'react'
-import classnames from 'classnames'
-import '../../utils/importIcons'
-import './style'
-
-const componentName = 'belial-icon'
+import * as React from "react";
+import classnames from "classnames";
+import "../../utils/importIcons";
+import "./style";
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
-  name: string
+  name: string;
+  spin?: boolean;
+  interval?: number;
 }
 
-const Icon: React.FC<IconProps> = props => {
-  const { name, className, ...rest }= props
+const iconPrefixCls: string = "belial-icon";
 
-  return <svg className={classnames(componentName, className)} { ...rest } >
-    <use xlinkHref={`#${name}`}/>
-  </svg>
-}
+const Icon: React.FC<IconProps> = (props) => {
+  const { name, spin, interval, className, ...restProps } = props;
 
-export default Icon
+  const classes = classnames(iconPrefixCls, className, {
+    [`${iconPrefixCls}-spin`]: spin || Boolean(interval),
+  });
+
+  return (
+    <svg
+      className={classes}
+      style={{
+        animationDuration: `${spin ? interval || 1800 : interval}ms`,
+      }}
+      {...restProps}
+    >
+      <use xlinkHref={`#${name}`} />
+    </svg>
+  );
+};
+
+export default Icon;
